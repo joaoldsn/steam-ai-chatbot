016ec977e4313c692665221fa0dd5faf91b692b9
João Leite da Silva Neto <joaolsn4@gmail.com>
2026-05-18 10:39:24 -0300

    Update memory.py

--- src/memory.py
+++ src/memory.py
@@ -1,1 +1,19 @@
+historico = []
 
+def adicionar_memoria(usuario, resposta):
+
+    historico.append(
+        f"Usuário: {usuario}"
+    )
+
+    historico.append(
+        f"GameGPT: {resposta}"
+    )
+
+
+def limpar_memoria():
+    historico.clear()
+
+
+def obter_historico():
+    return "\n".join(historico)
