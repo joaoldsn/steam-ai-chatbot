2c558d0075a418afc4111d54d339c08d063b8ec4
João Leite da Silva Neto <joaolsn4@gmail.com>
2026-05-18 10:47:23 -0300

    Update chain.py

--- src/chain.py
+++ src/chain.py
@@ -1,1 +1,21 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
+from chatbot.persona import SYSTEM_PROMPT
+from config.settings import llm
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", SYSTEM_PROMPT),
+    ("user", """
+Histórico:
+{historico}
+
+Dados do jogo:
+{contexto}
+
+Pergunta:
+{pergunta}
+""")
+])
+
+chain = prompt | llm | StrOutputParser()
